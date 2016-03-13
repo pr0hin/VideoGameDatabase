717767cf5da073519b1651d5d80fedade16cbc6c
r0e <rohinjpatel@gmail.com>
2016-03-13 13:49:35 -0700

    added example for UI

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,40 @@
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
 import java.sql.*;
 
-/**
- * Created by rohinpatel on 2016-02-24.
- */
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
+
+
             try {
                 DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
                 Connection con = DriverManager.getConnection(
@@ -15,6 +43,7 @@ public class Main {
                 System.out.println(sqle);
                 System.exit(-2);
             }
+
 
 
     }
