e663a1498380d56ebc5727993e3b1878dffbc99f
Spencer Lee <spencerlee1990@gmail.com>
2016-03-13 14:18:27 -0700

    getting login info from file

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.*;
 
 import javafx.application.Application;
@@ -8,6 +11,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
 
 public class Main extends Application{
     public void start(Stage primaryStage) {
@@ -34,11 +42,11 @@ public class Main extends Application{
     public static void main(String[] args) {
         launch(args);
 
-
+        String[] login = getOracleLogin();
             try {
                 DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
                 Connection con = DriverManager.getConnection(
-                        "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1521:ug", "username", "password");
+                        "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1521:ug", login[0], login[1]);
             } catch (SQLException sqle) {
                 System.out.println(sqle);
                 System.exit(-2);
@@ -48,4 +56,27 @@ public class Main extends Application{
 
     }
 
+    public static String[] getOracleLogin() {
+        String fileName = "./login.txt";
+        String[] loginInfo = new String[2];
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            for(int i = 0; i < loginInfo.length; i++){
+                String line = br.readLine();
+                if(line != null){
+                    loginInfo[i] = line;
+                }
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return loginInfo;
+
+    }
+
 }
+
+
