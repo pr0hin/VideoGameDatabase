a60e7b3c9b1675e7b997fa4b008c105f89d41ab5
r0e <rohinjpatel@gmail.com>
2016-03-22 17:01:31 -0700

    added test for connection for query

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ import java.sql.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import javax.sql.rowset.JdbcRowSet;
 import java.io.IOException;
 
 
@@ -45,14 +47,19 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         // This line needs to be at the end when we have the SQL connection working
-        launch(args);
+        //launch(args);
 
         String[] login = getOracleLogin();
         try {
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
             System.out.println("Setting up connection...");
             Connection con = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1521:ug", login[0], login[1]);
+                    "jdbc:oracle:thin:@localhost:1522:ug", login[0], login[1]);
+            Statement stm = con.createStatement();
+            ResultSet set = stm.executeQuery("SELECT * FROM gameupc");
+            while (set.next()) {
+                System.out.println(set.getString(2));
+        }
             System.out.println("Connected!");
         } catch (SQLException sqle) {
             System.out.println(sqle);
