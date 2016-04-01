00acca52099ade46cb9796edc8b04914e41fb340
Kevin Chiang <kevinchiang@dhcp-128-189-220-135.ubcsecure.wireless.ubc.ca>
2016-04-01 15:55:59 -0700

    Add employee search/update query to manager tab

--- src/controllers/EmployeeController.java
+++ src/controllers/EmployeeController.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import model.EmployeeModel;
 
 import java.net.URL;
@@ -10,6 +12,9 @@ import java.util.ResourceBundle;
  * Created by kevinchiang on 2016-03-19.
  */
 public class EmployeeController extends AbstractTabController implements Initializable {
+
+    @FXML
+    AnchorPane employeeTableViewContainer;
 
     public EmployeeController() {
         super(new EmployeeModel());
