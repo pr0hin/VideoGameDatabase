c3082b1910e168a3504fc19285bb0d079a4a2681
Spencer Lee <spencerlee1990@gmail.com>
2016-04-01 16:50:05 -0700

    added division query for employees that finds the titles of games that are on all platforms

--- src/controllers/EmployeeController.java
+++ src/controllers/EmployeeController.java
@@ -1,9 +1,28 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import model.EmployeeModel;
+import model.NotLoggedInException;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -18,4 +37,92 @@ public class EmployeeController extends AbstractTabController implements Initial
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
+
+
+    public void getGamesOnAllPlatforms(ActionEvent event){
+
+        EmployeeModel model = (EmployeeModel) getModel();
+        try {
+            ResultSet rs = model.executeGetGamesOnAllPlatforms();
+            if(rs == null){
+                createDialog("No games are on all platforms");
+            }
+            ResultSetParser rsparser = new ResultSetParser();
+            TableView tbl = rsparser.colparse(rs);
+            Stage stage = new Stage();
+            Scene tscene = new Scene(tbl);
+            stage.setTitle("RESULTS");
+            stage.setScene(tscene);
+            stage.show();
+        } catch (SQLException sqle){
+            createDialog(sqle.getMessage());
+        }
+    }
+
+    public void createDialog(String s) {
+        ButtonType okbutton = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        Dialog dialog = new Dialog();
+        dialog.getDialogPane().getButtonTypes().add(okbutton);
+        dialog.setContentText(s);
+        dialog.show();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
--- src/model/EmployeeModel.java
+++ src/model/EmployeeModel.java
@@ -1,7 +1,28 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by rohinpatel on 2016-03-13.
  */
 public class EmployeeModel extends AbstractModel {
+
+    public ResultSet executeGetGamesOnAllPlatforms() throws SQLException{
+        Statement stmt = this.getConn().createStatement();
+        ResultSet rs = stmt.executeQuery("select distinct title from gameupc G where not exists (select * from gameupc P where not exists (select * from gameupc C where C.platform = P.platform and C.upc = G.upc))");
+        return rs;
+    }
+
+
+
+
+
+
+
+
 }
+
+
+
