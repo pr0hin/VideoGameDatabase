938e2c21d0d4fef07beb47bd735b92640be68231
Kevin Chiang <kevinchiang@dhcp-128-189-220-135.ubcsecure.wireless.ubc.ca>
2016-04-01 18:27:39 -0700

    Add back the gamesOnAllPlatforms query Spencer made

--- src/controllers/EmployeeController.java
+++ src/controllers/EmployeeController.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.EmployeeModel;
-
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 /**
@@ -23,4 +28,20 @@ public class EmployeeController extends AbstractTabController implements Initial
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
+    public void getGamesOnAllPlatforms(ActionEvent event){
+
+        EmployeeModel model = (EmployeeModel) getModel();
+        try {
+            ResultSet rs = model.executeGetGamesOnAllPlatforms();
+            if(rs == null){
+                createDialog("No games are on all platforms");
+            }
+            ResultSetParser rsparser = new ResultSetParser();
+            TableView tbl = rsparser.colparse(rs);
+            addTable(tbl, employeeTableViewContainer);
+        } catch (SQLException sqle){
+            createDialog(sqle.getMessage());
+        }
+    }
+
 }
