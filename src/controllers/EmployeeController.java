e8bfd3f76efaa623859ad82bf262c5accf8d8f6e
amarvir doowa <amarvir doowa>
2016-04-01 19:05:46 -0700

    Add aggregation query for game count on platform

--- src/controllers/EmployeeController.java
+++ src/controllers/EmployeeController.java
@@ -1,13 +1,26 @@
 package controllers;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
+import model.CustomerModel;
 import model.EmployeeModel;
+
+import java.awt.*;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -44,4 +57,62 @@ public class EmployeeController extends AbstractTabController implements Initial
         }
     }
 
+    public void getCountOfGamesOnGivenPlat(ActionEvent event){
+
+        Stage stage = new Stage();
+        stage.setTitle("Game Search");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new javafx.geometry.Insets(25, 25, 25, 25));
+        Text scenetitle = new Text("Platform selection");
+        scenetitle.setFont(javafx.scene.text.Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Button btn = new Button("Search");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList(
+                "PC", "PS", "PS2", "PS3", "PS5", "WII", "DC", "XBOX")
+        );
+
+        grid.add(cb, 1, 4);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setText("Executing query");
+                EmployeeModel model = (EmployeeModel) getModel();
+                try {
+                    ResultSet rs = model.executeGetNumberOfGamesOnPlatform(cb.getValue().toString());
+                    if (rs == null) {
+                        createDialog("No games on specified platform");
+                    }
+                    ResultSetParser rsparser = new ResultSetParser();
+                    TableView tbl = rsparser.colparse(rs);
+
+                    addTable(tbl, employeeTableViewContainer);
+
+                } catch (SQLException sqle) {
+                    createDialog(sqle.getMessage());
+                }
+
+
+            }
+        });
+
+
+        Scene scene = new Scene(grid, 500, 300);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/model/EmployeeModel.java
+++ src/model/EmployeeModel.java
@@ -13,4 +13,11 @@ public class EmployeeModel extends AbstractModel {
         ResultSet rs = stmt.executeQuery("select distinct title from gameupc G where not exists (select * from gameupc P where not exists (select * from gameupc C where C.platform = P.platform and C.upc = G.upc))");
         return rs;
     }
+
+    public ResultSet executeGetNumberOfGamesOnPlatform (String platform) throws SQLException{
+
+        Statement stmt = this.getConn().createStatement();
+        ResultSet rs = stmt.executeQuery("select platform, count(title) from gameupc where platform = \'"+platform+"\' group by platform");
+        return rs;
+    }
 }
